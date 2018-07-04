8b8461030d26cdc866614ae9c7acbff1e4153688
Prashant Deepak <40815716+prashantdeepak@users.noreply.github.com>
2018-07-04 11:22:58 +0530

    Add a comment from github

--- handling.py
+++ handling.py
@@ -1,21 +1,21 @@
-try:
-    f = open('ac.txt')
-
-    for line in f:
-        print(line)
-    p = 4567
-    b = 546456
+#try:
+ #   f = open('ac.txt')
+#
+ #   for line in f:
+  #      print(line)
+ #   p = 4567
+  #  b = 546456
 
     #p = b/0
 
-except FileNotFoundError as e:
-    print(e.filename)
+#except FileNotFoundError as e:
+ #   print(e.filename)
 
-except Exception as e:
-    print(e)
+#except Exception as e:
+ #   print(e)
 
-except ZeroDivisionError as e:
-    print(e)
+#except ZeroDivisionError as e:
+ #   print(e)
 
 #except (FileNotFoundError, ZeroDivisionError) :
     #print('You should not have done that to me')
@@ -23,3 +23,5 @@ except ZeroDivisionError as e:
 #except ZeroDivisionError:
 #    print('Zero se divide mt na kr')
 # i = 0/0
+
+# we add git for fun
