56c2d0d16711d258d1329236fa1e9df0cb77e12e
prashantdeepak <deepakprashant325@gmail.com>
2018-07-04 10:53:31 +0530

    Error Handling Basic Code

---
+++ handling.py
@@ -0,0 +1,25 @@
+try:
+    f = open('ac.txt')
+
+    for line in f:
+        print(line)
+    p = 4567
+    b = 546456
+
+    #p = b/0
+
+except FileNotFoundError as e:
+    print(e.filename)
+
+except Exception as e:
+    print(e)
+
+except ZeroDivisionError as e:
+    print(e)
+
+#except (FileNotFoundError, ZeroDivisionError) :
+    #print('You should not have done that to me')
+ #   print('nOT FOUND FILE')
+#except ZeroDivisionError:
+#    print('Zero se divide mt na kr')
+# i = 0/0
